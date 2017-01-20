223152cfcc699948cfc16c0bf3cbdb349e6aaa8d
MartinB <mbeierling@gmail.com>
2017-01-20 23:01:18 +0100

    Added squeeze bottle

--- Assets/Code/SqueezeBottle.cs
+++ Assets/Code/SqueezeBottle.cs
@@ -2,15 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SqueezeBottle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class SqueezeBottle : MonoBehaviour
+{
+    [Header("Config")]
+    public SqueezeLine SqueezeLinePrefab;
+    public float PointsMinDistance = 0.1f;
+    public float UpOffset = 2.0f;
+
+    private SqueezeLine CurrentSqueezeLine;
+    private List<Vector3> CurrentPoints = new List<Vector3>();
+
+    private bool m_isSelected = true; // DEBUG: should start with false later and be set in SelectionManager
+    public bool IsSelected { get { return m_isSelected; } set { m_isSelected = value; } }
+
+    private ParticleSystem m_ParticleSystem;
+
+    private void Start()
+    {
+        m_ParticleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        // handle movement here when it is selected
+        if (IsSelected)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1 << 10)) // only on the "table" layer
+            {
+                transform.position = hit.point + Vector3.up * UpOffset;
+            }
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                // create the SqueezeLine object
+                CurrentSqueezeLine = Instantiate<SqueezeLine>(SqueezeLinePrefab);
+
+                // start particle effect
+                m_ParticleSystem.Play();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                // end particle effect
+                m_ParticleSystem.Stop();
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                // raycast from bottle to bottom 
+                if (Physics.Raycast(transform.position, Vector3.down, out hit))
+                {
+                    // check if current raycast target is further away from any currentPoints than minDistance
+                    if (IsPointFurtherAway(hit.point))
+                    {
+                        // add it to CurrentPoints and CurrentSqueezeLine.AddNewPoint
+                        CurrentPoints.Add(hit.point);
+                        StartCoroutine(AddNewPoint(CurrentSqueezeLine, hit.point));
+                        //CurrentSqueezeLine.AddNewPoint(hit.point);
+                    }
+                }
+            }
+        }        
+    }
+
+    IEnumerator AddNewPoint(SqueezeLine toLine, Vector3 point)
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        toLine.AddNewPoint(point);
+    }
+
+    private bool IsPointFurtherAway(Vector3 Point)
+    {
+        foreach(Vector3 p in CurrentPoints)
+        {
+            if (Vector3.Distance(p, Point) < PointsMinDistance)
+                return false;
+        }
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        CurrentPoints = new List<Vector3>();
+        CurrentSqueezeLine = null;
+    }
 }
---
+++ Assets/Code/SqueezeLine.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This class is only used for the visuals.
+ * When the bottle is squeezed, this object will be created
+ * and points are added, updating this mesh.
+ */
+
+public class SqueezeLine : MonoBehaviour
+{
+    [Header("Config")]
+    public Material Material;
+    public float Width = 0.25f;
+
+    private List<Vector3> Points = new List<Vector3>();
+
+    private MeshBuilder meshBuilder;
+    private Mesh currentMesh;
+
+    public void AddNewPoint(Vector3 Point)
+    {
+        Points.Add(Point);
+        UpdateMesh();
+    }
+
+    private void Start()
+    {
+        UpdateMesh();
+    }
+
+
+    private void UpdateMesh()
+    {
+        if (currentMesh != null)
+            Destroy(currentMesh);
+
+        MeshBuilder meshBuilder = new MeshBuilder();
+
+        // Add the vertices per point
+        for (int i = 0; i < Points.Count - 1; i++)
+        {
+            Vector3 forwardDir = Points[i + 1] - Points[i] ;
+
+            Vector3 rightDir = Vector3.Cross(forwardDir.normalized, Vector3.up.normalized);
+            Vector3 leftDir = -rightDir;
+
+            Vector3 forward = Points[i] + forwardDir;
+            Vector3 left = Points[i] + leftDir * Width;
+            Vector3 right = Points[i] + rightDir * Width;
+
+            // Root Location
+            meshBuilder.Vertices.Add(Points[i] + Vector3.up * 0.2f);
+            meshBuilder.UVs.Add(new Vector2(0.0f, 0.0f));
+            meshBuilder.Normals.Add(Vector3.up);
+
+            // Left from Root
+            meshBuilder.Vertices.Add(left);
+            meshBuilder.UVs.Add(new Vector2(0.0f, 1.0f));
+            meshBuilder.Normals.Add(Vector3.up);
+
+            // Forward from Root
+            meshBuilder.Vertices.Add(forward + Vector3.up * 0.2f);
+            meshBuilder.UVs.Add(new Vector2(1.0f, 1.0f));
+            meshBuilder.Normals.Add(Vector3.up);
+
+            // Right from Root
+            meshBuilder.Vertices.Add(right);
+            meshBuilder.UVs.Add(new Vector2(1.0f, 0.0f));
+            meshBuilder.Normals.Add(Vector3.up);
+
+            int vIndex = i * 4;
+            if (vIndex > 0)
+            {
+                // 0, 1' (-3), 1
+                meshBuilder.AddTriangle(vIndex, vIndex + 1, vIndex - 3);
+
+                // 0, 3, 3' (-1)
+                meshBuilder.AddTriangle(vIndex, vIndex - 1, vIndex + 3);
+            }
+            meshBuilder.AddTriangle(vIndex, vIndex + 2, vIndex + 1);
+            meshBuilder.AddTriangle(vIndex, vIndex + 3, vIndex + 2);
+        }
+
+        Mesh mesh = meshBuilder.CreateMesh();
+
+        MeshFilter filter = GetComponent<MeshFilter>();
+        if (filter != null)
+        {
+            filter.sharedMesh = mesh;
+        }
+    }
+}
