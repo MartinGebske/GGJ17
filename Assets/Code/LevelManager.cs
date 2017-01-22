b8165522e169df02d3bf9af4c1d89965540102c7
MartinB <mbeierling@gmail.com>
2017-01-22 02:48:51 +0100

    Fixes missing scripts in Start and LevelManager errors in Instructions

--- Assets/Code/LevelManager.cs
+++ Assets/Code/LevelManager.cs
@@ -17,26 +17,31 @@ public class LevelManager : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
 
-        LeanTween.moveY(Logo, -316f, 0.7f)
-            .setEase(LeanTweenType.easeOutElastic);
+        if (Logo != null)
+            LeanTween.moveY(Logo, -316f, 0.7f)
+                .setEase(LeanTweenType.easeOutElastic);
 
-        LeanTween.move(HotDogsImg, new Vector3(-670f, 377f), 1f)
-            .setEase(LeanTweenType.easeOutCubic);
-
-        yield return new WaitForSeconds(0.2f);
-
-        LeanTween.move(BtnPlay, new Vector3(217f, 452f), 0.7f)
-             .setEase(LeanTweenType.easeOutSine);
+        if (HotDogsImg != null)
+            LeanTween.move(HotDogsImg, new Vector3(-670f, 377f), 1f)
+                .setEase(LeanTweenType.easeOutCubic);
 
         yield return new WaitForSeconds(0.2f);
 
-        LeanTween.move(BtnInstr, new Vector3(206f, 153f), 0.7f)
-             .setEase(LeanTweenType.easeOutSine);
+        if (BtnPlay != null)
+            LeanTween.move(BtnPlay, new Vector3(217f, 452f), 0.7f)
+                 .setEase(LeanTweenType.easeOutSine);
 
         yield return new WaitForSeconds(0.2f);
 
-        LeanTween.move(BtnQuit, new Vector3(488f, 149f), 0.7f)
-             .setEase(LeanTweenType.easeOutSine);
+        if (BtnInstr != null)
+            LeanTween.move(BtnInstr, new Vector3(206f, 153f), 0.7f)
+                 .setEase(LeanTweenType.easeOutSine);
+
+        yield return new WaitForSeconds(0.2f);
+
+        if (BtnQuit != null)
+            LeanTween.move(BtnQuit, new Vector3(488f, 149f), 0.7f)
+                 .setEase(LeanTweenType.easeOutSine);
     }
 
 
