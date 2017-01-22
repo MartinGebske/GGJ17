93ab05948da5bf1e32d2ea3977f6b1719414cd20
MartinB <mbeierling@gmail.com>
2017-01-22 01:41:50 +0100

    Start-Animations

--- Assets/Code/LevelManager.cs
+++ Assets/Code/LevelManager.cs
@@ -5,6 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    [Header("Config")]
+    public RectTransform Logo;
+    public RectTransform HotDogsImg;
+    public RectTransform BtnPlay;
+    public RectTransform BtnInstr;
+    public RectTransform BtnQuit;
+
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        LeanTween.moveY(Logo, -316f, 0.7f)
+            .setEase(LeanTweenType.easeOutElastic);
+
+        yield return new WaitForSeconds(0.3f);
+
+        LeanTween.move(HotDogsImg, new Vector3(-670f, 377f), 1f)
+            .setEase(LeanTweenType.easeOutCubic);
+
+        yield return new WaitForSeconds(0.2f);
+
+        LeanTween.move(BtnPlay, new Vector3(217f, 452f), 0.7f)
+             .setEase(LeanTweenType.easeOutElastic);
+
+        yield return new WaitForSeconds(0.2f);
+
+        LeanTween.move(BtnInstr, new Vector3(206f, 153f), 0.7f)
+             .setEase(LeanTweenType.easeOutElastic);
+
+        yield return new WaitForSeconds(0.2f);
+
+        LeanTween.move(BtnQuit, new Vector3(488f, 149f), 0.7f)
+             .setEase(LeanTweenType.easeOutElastic);
+    }
+
+
     public void ClickOnButton(string name)
     {
         SceneManager.LoadScene(name);   
